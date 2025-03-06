9389f260a0a67c43d1afdcafa02fe9a51b126113
Alejandra Salinas <alejandra.salinas@ucb.edu.bo>
2025-03-06 00:48:55 -0400

    Segundo Saludo, con nombre

--- src/presenter.js
+++ src/presenter.js
@@ -15,9 +15,22 @@
 import {saludar1,saludar2} from "./saludador.js";
 
 const form1 = document.querySelector("#saludar1-form");
+const form2 = document.querySelector("#saludar2-form");
+
 
 form1.addEventListener("submit", (event) => {
   event.preventDefault();
   saludar1();
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombreInput = document.querySelector("#nombre").value.trim();
+  if (nombreInput !== "") {
+    saludar2(nombreInput);
+} else {
+    alert("Por favor, ingresa tu nombre.");
+}
 });
 
--- src/saludador.js
+++ src/saludador.js
@@ -2,3 +2,12 @@ export function saludar1() {
     alert('Hola Usuario!');
 }
  
+export function saludar2(name) {
+    const saludoMensaje = document.querySelector("#saludo-mensaje");
+    saludoMensaje.innerHTML = "";
+
+    const parrafo = document.createElement("p");
+    parrafo.textContent = `¡Hola, ${name}!`;
+
+    saludoMensaje.appendChild(parrafo);
+}
